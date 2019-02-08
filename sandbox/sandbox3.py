ee798395954aeced72590509fd515633244c9972
spannalg <spannalg@rose-hulman.edu>
2019-02-08 17:46:10 -0500

    Started to toy around with sandbox3

--- sandbox/sandbox3.py
+++ sandbox/sandbox3.py
@@ -1,2 +1,5 @@
 # Put whatever you want in this module and do whatever you want with it.
 # It exists here as a place where you can "try out" things without harm.
+# import tkinter
+# from tkinter import ttk
+# def frame = ttk.Frame(window, padding = 10, )
