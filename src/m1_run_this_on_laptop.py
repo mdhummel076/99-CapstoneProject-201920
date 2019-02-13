74868408822c1be84088062da09d67a2d4870be1
mdhummel076 <mdhummel076@gmail.com>
2019-02-13 15:33:23 -0500

    Implementing my own sprint 3

--- src/m1_run_this_on_laptop.py
+++ src/m1_run_this_on_laptop.py
@@ -53,7 +53,8 @@ def main():
     # Frames that are particular to my individual contributions to the project.
     # -------------------------------------------------------------------------
 
-    beepdriveFrame = get_my_frames(mainFrame,client)
+    beepdriveFrame = get_beepFrame(mainFrame,client)
+    simonFrame = getSimonFrame(mainFrame,client)
 
     # -------------------------------------------------------------------------
     # Grid the frames.
@@ -61,12 +62,13 @@ def main():
 
     teleopFrame.grid(row = 0, column = 0)
     armFrame.grid(row = 1, column = 0)
-    controlFrame.grid(row = 3, column = 0)
-    driveSystemFrame.grid(row = 4,column = 0)
-    soundmakerFrame.grid(row = 5, column = 0)
+    controlFrame.grid(row = 2, column = 0)
+    driveSystemFrame.grid(row = 3,column = 0)
+    soundmakerFrame.grid(row = 4, column = 0)
     colorFrame.grid(row=0,column=1)
     cameraFrame.grid(row=1,column=1)
     beepdriveFrame.grid(row=2,column=1)
+    simonFrame.grid(row=3,column=1)
 
 
     # -------------------------------------------------------------------------
@@ -90,15 +92,16 @@ def get_shared_frames(main_frame, mqtt_sender):
 def grid_frames(teleop_frame, arm_frame, control_frame):
     pass
 
-def get_my_frames(window,client):
+def get_beepFrame(window,client):
     # Construct the frame to return:
     frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
     frame.grid()
 
     # Construct the widgets on the frame:
-    frame_label = ttk.Label(frame, text="Beep Drive")
+    frame_label = ttk.Label(frame, text="Sprint 2 things")
 
     driveButton = ttk.Button(frame, text="Drive")
+    targetButton = ttk.Button(frame, text='Target')
     initBox = ttk.Entry(frame,width=8)
     rateBox = ttk.Entry(frame,width=8)
     initBox.insert(0,'1')
@@ -109,17 +112,58 @@ def get_my_frames(window,client):
     driveButton.grid(row=2,column=0)
     initBox.grid(row=2,column=1)
     rateBox.grid(row=2,column=2)
+    targetButton.grid(row=3,column=0)
 
     # Set the button callbacks:
-    driveButton['command'] = lambda: m1BeepDrive(client,initBox,rateBox)
+    driveButton['command'] = lambda: handleBeepDrive(client,initBox,rateBox)
+    targetButton['command'] = lambda: handleTarget(client)
 
     return frame
 
+def getSimonFrame(window,client):
 
-def m1BeepDrive(client,box1,box2):
+    Frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+    Frame.grid()
+
+    frameLabel = ttk.Label(Frame,text='Simon Says')
+    simonButton = ttk.Button(Frame,text='Simon Says...')
+    moveButton = ttk.Button(Frame,text='Move')
+    moveBox = ttk.Entry(Frame,width=8)
+    moveBox.insert(0,'5')
+    moveText = ttk.Label(Frame,text='Inches:')
+    danceButton = ttk.Button(Frame,text='Dance!')
+    stretchButton = ttk.Button(Frame,text='Stretch')
+    liftButton = ttk.Button(Frame,text='Lift')
+    findButton = ttk.Button(Frame,text='Find Cube')
+    blankLabel= ttk.Label(Frame,text='')
+    blankLabel2 = ttk.Label(Frame,text='')
+    diffLabel = ttk.Label(Frame,text='Difficulty Bar')
+    difficultBar = ttk.Progressbar(Frame,maximum=1000)
+    difficultBar.step(40)
+
+    frameLabel.grid(row=0,column=1)
+    blankLabel.grid(row=1,column=1)
+    simonButton.grid(row=2,column=1)
+    blankLabel2.grid(row=3,column=1)
+    moveButton.grid(row=4,column=0)
+    moveText.grid(row=4,column=1)
+    moveBox.grid(row=4,column=2)
+    danceButton.grid(row=5,column=0)
+    stretchButton.grid(row=6,column=0)
+    liftButton.grid(row=7,column=0)
+    findButton.grid(row=8,column=0)
+    diffLabel.grid(row=9,column=1)
+    difficultBar.grid(row=10,column=1)
+
+    return Frame
+
+def handleBeepDrive(client,box1,box2):
 
     client.send_message('m1BeepDrive',[box1.get(),box2.get()])
 
+def handleTarget(client):
+
+    client.send_message('centerOnTarget')
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
