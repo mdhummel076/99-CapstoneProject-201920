a7d80e7829ffa1d78f4a1e43dab15f5947a16253
spannalg <spannalg@rose-hulman.edu>
2019-02-20 13:47:07 -0500

    Updated my m3 module for a new personality

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -32,25 +32,6 @@ def real_deal():
     delegate = shared_gui_delegate_on_robot.Delegate(robot)
     receiver = com.MqttClient(delegate)
     receiver.connect_to_pc()
-def grab_yellow_object():
-    robot = rosebot.RoseBot()
-    delegate = shared_gui_delegate_on_robot.Delegate(robot)
-    robot.sensor_system.camera.set_signature("SIG1")
-    b1 = robot.sensor_system.camera.get_biggest_blob()
-    # robot.drive_system.spin_counterclockwise_until_sees_object(50,100)
-    # while robot.drive_system.spin_counterclockwise_until_sees_object(50,100):
-    #             robot.drive_system.go_forward_until_distance_is_less_than(1,50)
-    #             if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<10:
-    #                 robot.sound_system.speech_maker.speak("I am 10 inches away from target")
-    #             if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<5:
-    #                 robot.sound_system.speech_maker.speak("I am 5 inches away from target")
-    #             if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<1:
-    #                 robot.sound_system.speech_maker.speak("I now have the target")
-    #                 robot.arm_and_claw.raise_arm()
-    #                 robot.drive_system.go(-50,-50)
-    #                 if robot.sensor_system.color_sensor.get_color() == 6:
-    #                     robot.arm_and_claw.lower_arm()
-    print('Text')
 def flight():
     robot = rosebot.RoseBot()
     button = robot.sensor_system.touch_sensor
