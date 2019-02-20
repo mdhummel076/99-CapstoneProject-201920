531bfcd8c78387fcbfb8dcdd7a7ad8842691229d
spannalg <spannalg@rose-hulman.edu>
2019-02-19 20:58:11 -0500

    Updated my m3 module

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -18,7 +18,8 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicate via MQTT with the GUI code that runs on the LAPTOP.
     """
-    #real_deal()
+    print('Text2')
+    real_deal()
     grab_yellow_object()
 
 # -----------------------------------------------------------------------------
@@ -37,20 +38,21 @@ def grab_yellow_object():
     delegate = shared_gui_delegate_on_robot.Delegate(robot)
     robot.sensor_system.camera.set_signature("SIG1")
     b1 = robot.sensor_system.camera.get_biggest_blob()
-    robot.drive_system.spin_counterclockwise_until_sees_object(50,100)
-    while robot.drive_system.spin_counterclockwise_until_sees_object(50,100):
-                robot.drive_system.go_forward_until_distance_is_less_than(1,50)
-                if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<10:
-                    robot.sound_system.speech_maker.speak("I am 10 inches away from target")
-                if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<5:
-                    robot.sound_system.speech_maker.speak("I am 5 inches away from target")
-                if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<1:
-                    robot.sound_system.speech_maker.speak("I now have the target")
-                    robot.arm_and_claw.raise_arm()
-                    robot.drive_system.go(-50,-50)
-                    if robot.sensor_system.color_sensor.get_color() == 6:
-                        robot.arm_and_claw.lower_arm()
-    m1_run_this_on_robot.Delegate.grabIt(delegate)
+    # robot.drive_system.spin_counterclockwise_until_sees_object(50,100)
+    # while robot.drive_system.spin_counterclockwise_until_sees_object(50,100):
+    #             robot.drive_system.go_forward_until_distance_is_less_than(1,50)
+    #             if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<10:
+    #                 robot.sound_system.speech_maker.speak("I am 10 inches away from target")
+    #             if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<5:
+    #                 robot.sound_system.speech_maker.speak("I am 5 inches away from target")
+    #             if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<1:
+    #                 robot.sound_system.speech_maker.speak("I now have the target")
+    #                 robot.arm_and_claw.raise_arm()
+    #                 robot.drive_system.go(-50,-50)
+    #                 if robot.sensor_system.color_sensor.get_color() == 6:
+    #                     robot.arm_and_claw.lower_arm()
+    print('Text')
+
 def sprint_3():
     robot = rosebot.RoseBot()
 main()
