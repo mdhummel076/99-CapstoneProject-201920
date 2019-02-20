3eb173abbf95d04f45c70a5308b3e60efbc2ad65
spannalg <spannalg@rose-hulman.edu>
2019-02-20 15:30:44 -0500

    Updated my m3 module to test my fight response, made some tweaks with the code

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -78,7 +78,9 @@ def fight():
             robot.drive_system.go_forward_until_distance_is_less_than(1,50)
             speech.speak("Take this, you cur!")
             robot.arm_and_claw.raise_arm()
+            time.sleep(1)
             speech.speak("Get ready for the Grand Hand Slam!")
+            time.sleep(1)
             robot.arm_and_claw.lower_arm()
             speech.speak("Serves you right!")
             break
