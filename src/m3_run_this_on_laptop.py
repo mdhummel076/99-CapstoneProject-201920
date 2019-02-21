e9ed7f45014b15f48c7928e802c9cc403096fecf
spannalg <spannalg@rose-hulman.edu>
2019-02-21 16:22:24 -0500

    Updated my m3 module to test my flight response, made some tweaks with the code

--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -38,16 +38,14 @@ def main():
     # -------------------------------------------------------------------------
     # The main frame, upon which the other frames are placed.
     # -------------------------------------------------------------------------
-    main_frame = ttk.LabelFrame(padding = 10, borderwidth=5, relief = 'groove')
+    main_frame = ttk.LabelFrame( padding = 10, borderwidth=5, relief = 'groove')
     main_frame.grid()
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop, arm_frame, control_frame, SoundmakerFrame, drive_frame, ColorSensor_Frame, IR_Frame, \
-        camera_frame = get_shared_frames(main_frame, client)
-    sprint_2_frame = sprint_2_frames(main_frame, client)
-    grid_frames(teleop, arm_frame, control_frame, SoundmakerFrame, drive_frame, ColorSensor_Frame, IR_Frame, camera_frame,
-                sprint_2_frame)
+    teleop = get_shared_frames(main_frame, client)
+    sprint_3_frame = sprint_3_frames(main_frame, client)
+    grid_frames(teleop)
 
 
     # -------------------------------------------------------------------------
@@ -68,27 +66,27 @@ def main():
 
 def get_shared_frames(main_frame, mqtt_sender):
     teleop = shared_gui.get_teleoperation_frame(main_frame,mqtt_sender)
-    arm_frame = shared_gui.get_arm_frame(main_frame,mqtt_sender)
-    control_frame = shared_gui.get_control_frame(main_frame,mqtt_sender)
-    SoundmakerFrame = shared_gui.getSoundmakerFrame(main_frame,mqtt_sender)
-    drive_frame = shared_gui.get_drivesystem_frame(main_frame,mqtt_sender)
-    ColorSensor_Frame=shared_gui.get_ColorSensor_Frame(main_frame,mqtt_sender)
-    IR_Frame = shared_gui.get_IR_Sensor_Frame(main_frame,mqtt_sender)
-    camera_frame= shared_gui.get_camera_frame(main_frame,mqtt_sender)
+    # arm_frame = shared_gui.get_arm_frame(main_frame,mqtt_sender)
+    # control_frame = shared_gui.get_control_frame(main_frame,mqtt_sender)
+    # SoundmakerFrame = shared_gui.getSoundmakerFrame(main_frame,mqtt_sender)
+    # drive_frame = shared_gui.get_drivesystem_frame(main_frame,mqtt_sender)
+    # ColorSensor_Frame=shared_gui.get_ColorSensor_Frame(main_frame,mqtt_sender)
+    # IR_Frame = shared_gui.get_IR_Sensor_Frame(main_frame,mqtt_sender)
+    # camera_frame= shared_gui.get_camera_frame(main_frame,mqtt_sender)
 
-    return teleop, arm_frame, control_frame,SoundmakerFrame,drive_frame,ColorSensor_Frame,IR_Frame,camera_frame
+    return teleop
 
 
-def grid_frames(teleop_frame, arm_frame, control_frame,SoundmakerFrame,drive_frame,ColorSensor_Frame,IR_Frame,camera_frame, sprint_3_frame):
+def grid_frames(teleop_frame,  sprint_3_frame):
     teleop_frame.grid(row = 0, column =0)
-    arm_frame.grid(row=1, column=0)
-    control_frame.grid(row=2, column=0)
-    SoundmakerFrame.grid(row=0,column=1)
-    drive_frame.grid(row = 0, column = 2)
-    ColorSensor_Frame.grid(row=1,column=1)
-    IR_Frame.grid(row=1,column=2)
-    camera_frame.grid(row=2,column=1)
-    sprint_3_frame.grid(row=2, column=2)
+    # arm_frame.grid(row=1, column=0)
+    # control_frame.grid(row=2, column=0)
+    # SoundmakerFrame.grid(row=0,column=1)
+    # drive_frame.grid(row = 0, column = 2)
+    # ColorSensor_Frame.grid(row=1,column=1)
+    # IR_Frame.grid(row=1,column=2)
+    # camera_frame.grid(row=2,column=1)
+    sprint_3_frame.grid(row=1, column=0)
 
 
 def sprint_2_frames(window, mqtt_sender):
@@ -130,6 +128,24 @@ def handle_camera_proximity_led(mqtt_sender):
 
     print('Go to object & blink while driving')
     mqtt_sender.send_message('camera_proximity_led')
+def sprint_3_frames(main_frame,mqtt_sender):
+        sprint_3_label= ttk.Label(main_frame,text = 'Sprint 3 features')
+        sprint_3_label.grid(row=0,column=1)
+        flight_button = ttk.Button(main_frame,text = "Flight response", color = 'red')
+        flight_button.grid(row=0,column = 1)
+        fight_button = ttk.Button(main_frame, text='Fight response')
+        fight_button.grid(row=1,column=1)
+        flight_button['command']= lambda:handle_flight(mqtt_sender)
+        fight_button['command']= lambda:handle_fight(mqtt_sender)
+def handle_flight(mqtt_sender):
+    print("SENSORS DETECT HIGH PRESSURE")
+    mqtt_sender.send_message('flight')
+def handle_fight(mqtt_sender):
+    print("I'm getting mad!")
+    mqtt_sender.send_message('fight')
+
+
+
 
 
 
