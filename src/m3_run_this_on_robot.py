bd6307524309d08b04db32c37f173df05fd8e569
spannalg <spannalg@rose-hulman.edu>
2019-02-21 14:00:14 -0500

    Updated my m3 module to test my flight response, made some tweaks with the code

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -102,20 +102,17 @@ def fight():
                 speech.speak("Alright bub, you asked for it!")
                 robot.drive_system.go(50,-50)
                 time.sleep(2.8)
+                if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<10:
+                    speech.speak("Come here, chicken!")
+                robot.drive_system.go_forward_until_distance_is_less_than(2,50)
+                speech.speak("Take this, you cur!")
+                robot.arm_and_claw.raise_arm()
+                time.sleep(1)
+                speech.speak("Get ready for the Grand Hand Slam!")
+                time.sleep(3)
+                robot.arm_and_claw.lower_arm()
+                speech.speak("Serves you right, you jerk!")
                 break
-    robot.drive_system.go(50,50)
-    while True:
-        if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()<10:
-            speech.speak("Come here, chicken!")
-            robot.drive_system.go_forward_until_distance_is_less_than(2,50)
-            speech.speak("Take this, you cur!")
-            robot.arm_and_claw.raise_arm()
-            time.sleep(1)
-            speech.speak("Get ready for the Grand Hand Slam!")
-            time.sleep(3)
-            robot.arm_and_claw.lower_arm()
-            speech.speak("Serves you right, you jerk!")
-            break
 
 
 
