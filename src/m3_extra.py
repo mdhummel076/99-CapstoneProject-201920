065a0803f1503e1bb800a0c5931c6c437dae9065
spannalg <spannalg@rose-hulman.edu>
2019-02-22 11:15:46 -0500

    IMproved functions for the GUI on delegate

--- src/m3_extra.py
+++ src/m3_extra.py
@@ -1,6 +1,6 @@
 import time
 import rosebot
-def flight(self):
+def flight(robot):
     robot = rosebot.RoseBot()
     button = robot.sensor_system.touch_sensor
     speech = robot.sound_system.speech_maker
@@ -47,37 +47,37 @@ def flight(self):
 
         time.sleep(1)
 
-    def fight(self):
-        robot = rosebot.RoseBot()
-        button = robot.sensor_system.touch_sensor
-        speech = robot.sound_system.speech_maker
-        counter = 0
-        counter_1 = 0
-        counter_2 = 0
-        counter_3 = 0
-        while True:
-            if button.is_pressed() == True:
-                counter = counter + 1
-            if counter == 1:
-                counter_1 = counter_1 + 1
-                if counter_1 == 1:
-                    speech.speak("Hey! Watch what you're doing, twerp!")
-            if counter == 2:
-                counter_2 = counter_2 + 1
-                if counter_2 == 1:
-                    speech.speak("You'll be gettin a knuckle sandwich, punk!")
-            if counter == 3:
-                counter_3 = counter_3 + 1
-                if counter_3 == 1:
-                    speech.speak("Alright bub, you asked for it!")
-                    break
-        robot.drive_system.go(50, -50)
-        time.sleep(2.8)
-        robot.drive_system.go_forward_until_distance_is_less_than(2, 50)
-        speech.speak("Take this, you cur!")
-        robot.arm_and_claw.raise_arm()
-        time.sleep(1)
-        speech.speak("Get ready for the Grand Hand Slam!")
-        time.sleep(3)
-        robot.arm_and_claw.lower_arm()
-        speech.speak("Serves you right, you jerk!")
+def fight(robot):
+    robot = rosebot.RoseBot()
+    button = robot.sensor_system.touch_sensor
+    speech = robot.sound_system.speech_maker
+    counter = 0
+    counter_1 = 0
+    counter_2 = 0
+    counter_3 = 0
+    while True:
+        if button.is_pressed() == True:
+            counter = counter + 1
+        if counter == 1:
+            counter_1 = counter_1 + 1
+            if counter_1 == 1:
+                speech.speak("Hey! Watch what you're doing, twerp!")
+        if counter == 2:
+            counter_2 = counter_2 + 1
+            if counter_2 == 1:
+                speech.speak("You'll be gettin a knuckle sandwich, punk!")
+        if counter == 3:
+            counter_3 = counter_3 + 1
+            if counter_3 == 1:
+                speech.speak("Alright bub, you asked for it!")
+                break
+    robot.drive_system.go(50, -50)
+    time.sleep(2.8)
+    robot.drive_system.go_forward_until_distance_is_less_than(2, 50)
+    speech.speak("Take this, you cur!")
+    robot.arm_and_claw.raise_arm()
+    time.sleep(1)
+    speech.speak("Get ready for the Grand Hand Slam!")
+    time.sleep(3)
+    robot.arm_and_claw.lower_arm()
+    speech.speak("Serves you right, you jerk!")
